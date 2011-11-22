e184327fb8dd7200e8c7985997f3c6a05fef573e
Andrew Crump <andrew.crump@ieee.org>
2011-11-22 23:07:31 +0000

    Use pcap2json package, refs #1.

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,1 +1,3 @@
 default[:huchen][:capture_file] = "#{Chef::Config[:file_cache_path]}/huchen.json"
+default[:huchen][:pcap2json] = Mash.new
+default[:huchen][:pcap2json][:version] = '0.1.0'
--- recipes/default.rb
+++ recipes/default.rb
@@ -1,13 +1,16 @@
 %x{nohup --version}
 raise RuntimeError.new("huchen requires the nohup utility") unless $?.success?
 
-cookbook_file "/usr/bin/pcap2json" do
-  source "pcap2json"
-  mode "0700"
+pkg_path = "/huchen/files/default/pcap2json_#{node[:huchen][:pcap2json][:version]}_i386.deb"
+Chef::Config[:cookbook_path].each do |cbk_path|
+  path = File.join(cbk_path, pkg_path)
+  dpkg_package "pcap2json" do
+    source path
+    action :install
+    only_if {File.exists? path}
+  end
 end
 
-execute "pcap2json" do
-  command "nohup /usr/bin/pcap2json > '#{node[:huchen][:capture_file]}' &"
-end
+execute "nohup /usr/bin/pcap2json > '#{node[:huchen][:capture_file]}' &"
 
 include_recipe 'huchen::matchers'
